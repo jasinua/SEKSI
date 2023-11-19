a66e51ac4d9c1ac6837dd072cecb4ba6c38a5603
jasinua <115897385+jasinua@users.noreply.github.com>
2023-11-19 17:47:34 +0100

    File

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sistemi Elektronik per Komunikimin e Studenteve"
+include(":app")
+ 
